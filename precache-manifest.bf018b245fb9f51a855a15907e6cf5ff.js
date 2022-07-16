59f451b1be596367f39496e12bdbfead4e03a39b
Onitiu Ruben-Felix <92886844+RubyFili94@users.noreply.github.com>
2022-07-16 20:34:53 +0300

    Add files via upload

---
+++ precache-manifest.bf018b245fb9f51a855a15907e6cf5ff.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "cdfa4c83d3b90559260b",
+    "url": "/static/js/main.cdfa4c83.chunk.js"
+  },
+  {
+    "revision": "bf4f215fd5a24fb4644a",
+    "url": "/static/js/1.bf4f215f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1feb6a73d90869d4aac381bad1887da6",
+    "url": "/index.html"
+  }
+];
